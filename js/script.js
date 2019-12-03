dbd22b3eed139b69ec0450ad9e1cb34fa108b59a
James Pearce <jamesquintanapearce@gmail.com>
2019-12-03 18:56:29 +1100

    added save icons, and moved ID to row

--- js/script.js
+++ js/script.js
@@ -22,25 +22,43 @@ function showDate() {
 function setCurrentHour() {
 
     var currentHour = moment().format("H"); // moment() returns the WHOLE time, date, location. with format("H") it only returns the hour section in 24hour times.
-    console.log(currentHour); // do I need to though? I should be able to just set it and compare it. Don't need specifically hour. 
+    console.log(currentHour); 
 
 
-        $( ".time-block" ).each(function() {
-           var itemID = parseInt(this.id);
-           console.log(itemID);
-          if ( itemID == currentHour ) {
-            $(this).addClass("present");
-            console.log("present");
-          } else if ( itemID < currentHour){
-            $(this).addClass("past");
-            console.log("past");
-          }
-          else {
-              $(this).addClass("future");
-              console.log("future");
-          }
-          console.log("iteration");
-        });
+    $( ".time-block" ).each(function() {
+        var itemID = parseInt($(this).parent().attr("id"));
+        console.log(itemID);
+       if ( itemID == currentHour ) {
+         $(this).addClass("present");
+         console.log("present");
+       } else if ( itemID < currentHour){
+         $(this).addClass("past");
+         console.log("past");
+       }
+       else {
+           $(this).addClass("future");
+           console.log("future");
+       }
+       console.log("iteration");
+     });
+
+    
+        // $( ".time-block" ).each(function() {
+        //    var itemID = parseInt(this.id);
+        //    console.log(itemID);
+        //   if ( itemID == currentHour ) {
+        //     $(this).addClass("present");
+        //     console.log("present");
+        //   } else if ( itemID < currentHour){
+        //     $(this).addClass("past");
+        //     console.log("past");
+        //   }
+        //   else {
+        //       $(this).addClass("future");
+        //       console.log("future");
+        //   }
+        //   console.log("iteration");
+        // });
     
 
 
